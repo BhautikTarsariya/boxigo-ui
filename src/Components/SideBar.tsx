58ebd88aae5bfc0ef52202854fc195de160b1ee4
BhautikTarsariya <bhautiktarsariya@gmail.com>
2024-07-21 15:53:06 +0530

    code optimization

--- src/Components/SideBar.tsx
+++ src/Components/SideBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import {
   IconTruckDelivery,
@@ -8,6 +8,27 @@ import {
 } from "@tabler/icons-react";
 
 const Sidebar = () => {
+  const [open, setOpen] = useState<boolean>(true);
+
+  const menuItems = [
+    {
+      icons: <IconTruckDelivery stroke={2} />,
+      label: "MY MOVES",
+    },
+    {
+      icons: <IconUser stroke={2} />,
+      label: "MY PROFILE",
+    },
+    {
+      icons: <IconMessageQuestion stroke={2} />,
+      label: "GET QUOTE",
+    },
+    {
+      icons: <IconLogout stroke={2} />,
+      label: "LOGOUT",
+    },
+  ];
+
   return (
     <aside
       id="logo-sidebar"
@@ -16,33 +37,21 @@ const Sidebar = () => {
     >
       <div className="h-full px-8 py-10 flex flex-col items-center">
         <ul>
-          <li>
-            <Link
-              to="/"
-              className="px-4 mb-8 flex items-center flex items-center border-l-4 border-red-600 -ml-1"
-            >
-              <IconTruckDelivery stroke={2} />
-              <span className="ms-3 font-bold">MY MOVES</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/" className="px-4 mb-8 flex items-center">
-              <IconUser stroke={2} />
-              <span className="ms-3 font-bold">MY PROFILE</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/" className="px-4 mb-8 flex items-center">
-              <IconMessageQuestion stroke={2} />
-              <span className="ms-3 font-bold">GET QUOTE</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/" className="px-4 mb-8 flex items-center">
-              <IconLogout stroke={2} />
-              <span className="ms-3 font-bold">LOGOUT</span>
-            </Link>
-          </li>
+          {menuItems.map((page: any, index: number) => (
+            <li key={index}>
+              <Link
+                to="/"
+                className={`px-4 mb-8 flex items-center ${
+                  index === 0
+                    ? "flex items-center border-l-4 border-red-600 -ml-1"
+                    : ""
+                }`}
+              >
+                {page.icons}
+                <span className="ms-3 font-bold">{page.label}</span>
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </aside>
